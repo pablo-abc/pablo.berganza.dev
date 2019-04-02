ea1753f35d92e8703b77bfdba8f3081b53c5a51f
Pablo Berganza <pberganza@applaudostudios.com>
2019-04-02 11:53:02 -0600

    :bug: Update lang link

--- src/cljs/site/core.cljs
+++ src/cljs/site/core.cljs
@@ -1,9 +1,20 @@
 (ns site.core
-  (:require ["@barba/core" :as barba]))
+  (:require ["@barba/core" :as barba]
+            [clojure.string :as string]))
+
+(defn update-lang-link! [data]
+  (let [path (.-path (.-url (.-next data)))
+        lang-nav (.getElementById js/document "lang-nav")
+        new-lang (.-innerHTML lang-nav)
+        new-path (if (= new-lang "en")
+                   (string/replace path #"^/es" "")
+                   (str "/es" path))]
+    (set! (.-href lang-nav) new-path)))
 
 (defn barba-init! []
   (.init barba (clj->js {:transitions
-                        [{:enter (fn [_] (.scrollTo js/window 0 0))}]})))
+                        [{:leave (fn [_] (.scrollTo js/window 0 0))
+                          :enter update-lang-link!}]})))
 
 (defn init! [& args]
   (barba-init!))
