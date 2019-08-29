338cc1242f051a47ebc8c8dab133103eecf3d46c
Pablo Berganza <pberganza10@gmail.com>
2019-08-29 00:16:08 -0600

    :sparkles: Add preferred language

--- src/cljs/site/core.cljs
+++ src/cljs/site/core.cljs
@@ -2,6 +2,9 @@
   (:require ["@barba/core" :as barba]
             [clojure.string :as string]))
 
+(defn current-lang []
+  (.. js/document -documentElement -lang))
+
 (defn update-lang-link! [data]
   (let [path (.-path (.-url (.-next data)))
         lang-nav (.getElementById js/document "lang-nav")
@@ -16,5 +19,23 @@
                         [{:leave (fn [_] (.scrollTo js/window 0 0))
                           :enter update-lang-link!}]})))
 
+(defn redirect-lang! []
+  (let [pref-lang (.getItem js/localStorage "pref-lang")
+        curr-lang (current-lang)
+        url (js/URL. (.-href js/location))
+        pathname (.-pathname url)]
+    (when (and pref-lang (not= pref-lang curr-lang))
+      (if (= pref-lang "es")
+        (set! (.-pathname url) (str "/es" pathname))
+        (set! (.-pathname url) (string/replace pathname #"^/es" "")))
+      (.replace js/location (.-href url)))))
+
+(defn set-pref-lang! []
+  (let [curr-lang (current-lang)
+        pref-lang (if (= curr-lang "en") "es" "en")]
+    (.setItem js/localStorage "pref-lang" pref-lang)))
+
 (defn init! [& args]
+  (redirect-lang!)
+  (set! (.-onclick (.getElementById js/document "lang-nav")) set-pref-lang!)
   (barba-init!))
