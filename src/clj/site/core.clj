675784a566015e1eaca990e9f96f5b9638372813
Pablo Berganza <pberganza@applaudostudios.com>
2019-09-03 10:10:15 -0600

    :sparkles: Add link to sivar.cafe profile

--- src/clj/site/core.clj
+++ src/clj/site/core.clj
@@ -91,6 +91,7 @@
     [:head
      [:meta {:charset "utf-8"}]
      [:link {:href "https://toot.cafe/@pberganza" :rel "me"}]
+     [:link {:href "https://sivar.cafe/@pablo" :rel "me"}]
      ;; FAVICON
      [:link {:rel "apple-touch-icon", :sizes "57x57", :href "/apple-icon-57x57.png"}]
      [:link {:rel "apple-touch-icon", :sizes "60x60", :href "/apple-icon-60x60.png"}]
